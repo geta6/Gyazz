22a39e4a195f712963dfe83079c031c7d45d6156
Toshiyuki Masui <masui@pitecan.com>
2014-07-01 09:38:25 +0900

    models, controllersを作成して分離

---
+++ controllers/main.js
@@ -0,0 +1,65 @@
+(function() {
+  var Gyazz, debug, mongoose;
+
+  mongoose = require('mongoose');
+
+  Gyazz = mongoose.model('pages');
+
+  module.exports = function(app) {
+    app.get('/', function(req, res) {
+      res.render('index', { title: 'Express' });
+    });
+
+    app.get('/:wiki/:title',function(req, res) {
+      console.log('Getting wiki/title');
+      wiki = req.params.wiki;
+      title = req.params.title;
+      console.log("wiki= " + wiki);
+      console.log("title= " + title);
+      //Gyazz.find({wiki:req.params.wiki, title:req.params.title}, function(err, results) {
+      //Gyazz.find({'title':title}, function(err, results) {
+      Gyazz.find({'wiki':wiki, 'title':title},{},{sort:{timestamp: -1},limit:1}, function(err, results){ // 最新のをひとつだけ取得
+        if (err) {
+          res.send({'error': 'An error has occurred'});
+        } else {
+          console.log('Success: Getting GyazzData');
+          //res.render('index', { title: req.params.title });
+          res.render('page', { title: title});
+          console.log(results.length);
+          console.log(results[0].timestamp);
+          //console.log(results[results.length-1].timestamp);
+          //res.render('index', { title: 'xxxx'});
+        }
+      });
+    });
+    
+    app.get('/:wiki/:title/json',function(req, res) {
+      console.log('Getting wiki/title/json');
+      wiki = req.params.wiki;
+      title = req.params.title;
+      
+      // var url_parts = url.parse(req.url,true);
+      // console.log(url_parts.query);
+      console.log(req.query);
+      
+      Gyazz.find({'wiki':wiki, 'title':title},{},{sort:{timestamp: -1},limit:1}, function(err, results){ // 最新のをひとつだけ取得
+        if (err) {
+          res.send({'error': 'An error has occurred'});
+        } else {
+          // console.log(results[0].text);
+          data = {};
+          data['date'] = '20140101010101';
+          data['age'] = results[0].timestamp;
+          data['data'] = results[0].text.split(/\n/);
+          //res.send(JSON.stringify(data));
+          res.send({
+            'date': '20140101010101',
+            'age': results[0].timestamp,
+            'data': results[0].text.split(/\n/)
+          });
+          // res.render('index', { title: results.length});
+        }
+      });
+    });
+  };
+}).call(this);
---
+++ models/page.js
@@ -0,0 +1,18 @@
+(function(){
+
+  var mongoose = require('mongoose');
+
+  module.exports = function(app) {
+
+    var Gyazz;
+    var pageSchema = new mongoose.Schema({
+      wiki: String,
+      title: String,
+      text: String,
+      timestamp: Date
+    });
+
+    return Gyazz = mongoose.model('pages', pageSchema);
+  };
+
+}).call(this);
