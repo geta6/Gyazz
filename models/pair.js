3b8f7858ca28670597431534c3ecc01934a9c3a5
Toshiyuki Masui <masui@pitecan.com>
2014-07-04 11:35:43 +0900

    ...

---
+++ models/pair.js
@@ -0,0 +1,22 @@
+//
+// リンク情報
+//
+(function(){
+
+  var mongoose = require('mongoose');
+
+  module.exports = function(app) {
+
+    var Pairs;
+    var pairSchema = new mongoose.Schema({
+      wiki: String,
+      title1: String,
+      title2: String
+    }, {
+      collection:"Pairs" // Mongooseは勝手に小文字の複数形にするので大文字を使うときはこういう指定が必要
+    });
+
+    return Pairs = mongoose.model('Pairs', pairSchema);
+  };
+
+}).call(this);
